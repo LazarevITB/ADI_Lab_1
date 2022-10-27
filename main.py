c572c40a7f272a84fe3c01525edd0264a2962cf7
dmtrlzrv <lazarevitb@gmail.com>
2022-10-27 12:19:44 +0300

    Переработка данных

--- main.py
+++ main.py
@@ -8,4 +8,22 @@ data = pd.read_csv('Data/salary-train.csv')
 test = pd.read_csv('Data/salary-test-mini.csv')
 data.head()
 test
+
+#Проверка чтения
 print(' Data length: ', len(data), '\n Test length', len(test))
+
+#Переработка данных
+def transform_data(data):
+    data.FullDescription = data.FullDescription.replace('[^a-zA-Z0-9]', ' ', regex=True).str.lower()
+    return data
+
+data = transform_data(data)
+test = transform_data(test)
+
+data.head()
+
+vectorizer = TfidfVectorizer(min_df=5)
+tf_idf = vectorizer.fit_transform(data.FullDescription)
+
+print(tf_idf[:5])
+print('\n\n', tf_idf.shape)
